fbf826a0437df0ed7ca738de7c9240506d1700e8
Jamal Hussein <jamal.hussein@aditro.com>
2022-03-07 15:40:23 +0100

    added first controller with get and moved over c# code to seperated partial

---
+++ Blecommerce/Client/Shared/ProductList.razor.cs
@@ -0,0 +1,20 @@
+﻿using Blecommerce.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace Blecommerce.Client.Shared
+{
+    public partial class ProductList
+    {
+        [Inject]
+        protected HttpClient client { get; set; } = default!;
+
+        private static List<Product> Products = new List<Product>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var result = await client.GetFromJsonAsync<List<Product>>("api/product");
+            if (result != null) Products = result;
+        }
+    }
+}
---
+++ Blecommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blecommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+                Id = 1,
+                Title = "Onda drömmar",
+                Description = "Amelie Muscat arbetar som barnpsykolog. På en kurs har hon träffat Laura och blivit blixtförälskad. De flyttar ihop utan att egentligen känna varandra. När Amelie ska passa Lauras treårige son gör hon ett ödesdigert misstag. Sedan är pojken försvunnen",
+                ImageUrl = "https://images.nextory.com/9789113101569.jpg?fit=clip&w=340",
+                Price = 9.99m
+            },
+            new Product
+            {
+                Id = 2,
+                Title ="Depphjärnan",
+                Description = "Succéförfattaren och psykiatern Anders Hansen är tillbaka med en ny bok om hjärnan, där han reder ut varför vi mår så dåligt när vi har det så bra",
+                ImageUrl = "https://images.nextory.com/9789178873708.jpg?fit=clip&w=340",
+                Price = 9.99m
+            },
+            new Product
+            {
+                Id = 3,
+                Title ="I dina händer",
+                Description = "Det finns böcker som är så starka att man inte kan värja sig. ”I dina händer” av Malin Persson Giolito är en sådan bok.",
+                ImageUrl = "https://images.nextory.com/9789146238393.jpg?fit=clip&w=340",
+                Price = 9.99m
+            }
+        };
+        
+        [HttpGet]  
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(Products);
+        }
+    }
+}
