b776af5499f9ee3368425b4bddf4b46bcad73881
Jamal Hussein <jamal.hussein@aditro.com>
2022-03-10 18:16:58 +0100

    add more tables and properties

--- Blecommerce/Server/Data/DataContext.cs
+++ Blecommerce/Server/Data/DataContext.cs
@@ -9,9 +9,28 @@
 
         public DbSet<Product> Products { get; set; } = default!;
         public DbSet<Category> Categories { get; set; } = default!;
+        public DbSet<ProductVariant> ProductVariants { get; set; } = default!;
+        public DbSet<ProductType> ProductTypes { get; set; } = default!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<ProductVariant>()
+                .HasKey(x => new { x.ProductId ,x.ProductTypeId});
+
+
+            modelBuilder.Entity<ProductType>().HasData(
+                    new ProductType { Id = 1, Name = "Default" },
+                    new ProductType { Id = 2, Name = "Paperback" },
+                    new ProductType { Id = 3, Name = "E-Book" },
+                    new ProductType { Id = 4, Name = "Audiobook" },
+                    new ProductType { Id = 5, Name = "Stream" },
+                    new ProductType { Id = 6, Name = "Blu-ray" },
+                    new ProductType { Id = 7, Name = "VHS" },
+                    new ProductType { Id = 8, Name = "PC" },
+                    new ProductType { Id = 9, Name = "PlayStation" },
+                    new ProductType { Id = 10, Name = "Xbox" }
+                );
+
             modelBuilder.Entity<Category>().HasData(
                 new Category
                 {
@@ -42,7 +61,7 @@
                     Title = "Onda drömmar",
                     Description = "Amelie Muscat arbetar som barnpsykolog. På en kurs har hon träffat Laura och blivit blixtförälskad. De flyttar ihop utan att egentligen känna varandra. När Amelie ska passa Lauras treårige son gör hon ett ödesdigert misstag. Sedan är pojken försvunnen",
                     ImageUrl = "https://images.nextory.com/9789113101569.jpg?fit=clip&w=340",
-                    Price = 9.99m,
+                   // Price = 9.99m,
                     CategoryId = 1,
                 },
                 new Product
@@ -51,7 +70,7 @@
                     Title = "Depphjärnan",
                     Description = "Succéförfattaren och psykiatern Anders Hansen är tillbaka med en ny bok om hjärnan, där han reder ut varför vi mår så dåligt när vi har det så bra",
                     ImageUrl = "https://images.nextory.com/9789178873708.jpg?fit=clip&w=340",
-                    Price = 9.99m,
+                    //Price = 9.99m,
                     CategoryId = 1,
                 },
                 new Product
@@ -60,14 +79,14 @@
                     Title = "I dina händer",
                     Description = "Det finns böcker som är så starka att man inte kan värja sig. ”I dina händer” av Malin Persson Giolito är en sådan bok.",
                     ImageUrl = "https://images.nextory.com/9789146238393.jpg?fit=clip&w=340",
-                    Price = 9.99m,
+                    //Price = 9.99m,
                     CategoryId = 1,
                 },
                 new Product
                 {
                     Id = 4,
                     CategoryId = 2,
-                    Price = 4.99m,
+                    //Price = 4.99m,
                     Title = "The Matrix",
                     Description = "The Matrix is a 1999 science fiction action film written and directed by the Wachowskis, and produced by Joel Silver. Starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano, and as the first installment in the Matrix franchise, it depicts a dystopian future in which humanity is unknowingly trapped inside a simulated reality, the Matrix",
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
@@ -76,7 +95,7 @@
                 {
                     Id = 5,
                     CategoryId = 2,
-                    Price = 3.99m,
+                    //Price = 3.99m,
                     Title = "Back to the Future",
                     Description = "Back to the Future is a 1985 American science fiction film directed by Robert Zemeckis. Written by Zemeckis and Bob Gale, it stars Michael J. Fox, Christopher Lloyd, Lea Thompson, Crispin Glover, and Thomas F. Wilson. Set in 1985, the story follows Marty McFly (Fox), a teenager accidentally sent back to 1955 in a time-traveling DeLorean automobile built by his eccentric scientist friend Doctor Emmett  Brown (Lloyd)", 
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg",
@@ -85,7 +104,7 @@
                 {
                     Id = 6,
                     CategoryId = 2,
-                    Price = 2.99m,
+                    //Price = 2.99m,
                     Title = "Toy Story",
                     Description = "Toy Story is a 1995 American computer-animated comedy film produced by Pixar Animation Studios and released by Walt Disney Pictures. The first installment in the Toy Story franchise, it was the first entirely computer-animated feature film, as well as the first feature film from Pixar. The film was directed by John Lasseter (in his feature directorial debut), and written by Joss Whedon, Andrew Stanton, Joel Cohen, and Alec Sokolow from a story by Lasseter,", 
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
@@ -96,7 +115,7 @@
                     Id = 7,
                     CategoryId = 3,
                     Title = "Half-Life 2",
-                    Price = 49.99m,
+                    //Price = 49.99m,
                     Description = "Half-Life är en serie datorspel med science fiction-tema. Alla spel är förstapersonsskjutare, linjära och designade för en spelare. Spelen i serien har utvecklats av Valve Corporation, Gearbox Software och Taito Corporation. Det första spelet, Half-Life, var Valves första titel och släpptes i november 1998.",
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg",
 
@@ -106,7 +125,7 @@
                     Id = 8,
                     CategoryId = 3,
                     Title = "Diablo II",
-                    Price = 9.99m,
+                    //Price = 9.99m,
                     Description = "Diablo II is an action role-playing hack-and-slash computer video game developed by Blizzard North and published by Blizzard Entertainment in 2000 for Microsoft Windows, Classic Mac OS, and macOS.",
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/en/d/d5/Diablo_II_Coverart.png",
                 },
@@ -114,7 +133,7 @@
                 {
                     Id = 9,
                     CategoryId = 3,
-                    Price = 14.99m,
+                    //Price = 14.99m,
                     Title = "Day of the Tentacle",
                     Description = "Day of the Tentacle, also known as Maniac Mansion II: Day of the Tentacle, is a 1993 graphic adventure game developed and published by LucasArts. It is the sequel to the 1987 game Maniac Mansion.",
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/en/7/79/Day_of_the_Tentacle_artwork.jpg",
@@ -123,7 +142,7 @@
                 {
                     Id = 10,
                     CategoryId = 3,
-                    Price = 159.99m,
+                    //Price = 159.99m,
                     Title = "Xbox",
                     Description = "The Xbox is a home video game console and the first installment in the Xbox series of video game consoles manufactured by Microsoft.", 
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/4/43/Xbox-console.jpg",
@@ -132,10 +151,123 @@
                 {
                     Id = 11,
                     CategoryId = 3,
-                    Price = 79.99m,
+                    //Price = 79.99m,
                     Title = "Super Nintendo Entertainment System",
                     Description = "The Super Nintendo Entertainment System (SNES), also known as the Super NES or Super Nintendo, is a 16-bit home video game console developed by Nintendo that was released in 1990 in Japan and South Korea.",
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/ee/Nintendo-Super-Famicom-Set-FL.jpg",
+                }
+            );
+
+            modelBuilder.Entity<ProductVariant>().HasData(
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 2,
+                    Price = 9.99m,
+                    OriginalPrice = 19.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 3,
+                    Price = 7.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 4,
+                    Price = 19.99m,
+                    OriginalPrice = 29.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 2,
+                    ProductTypeId = 2,
+                    Price = 7.99m,
+                    OriginalPrice = 14.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 3,
+                    ProductTypeId = 2,
+                    Price = 6.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 5,
+                    Price = 3.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 6,
+                    Price = 9.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 7,
+                    Price = 19.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 5,
+                    ProductTypeId = 5,
+                    Price = 3.99m,
+                },
+                new ProductVariant
+                {
+                    ProductId = 6,
+                    ProductTypeId = 5,
+                    Price = 2.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 8,
+                    Price = 19.99m,
+                    OriginalPrice = 29.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 9,
+                    Price = 69.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 10,
+                    Price = 49.99m,
+                    OriginalPrice = 59.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 8,
+                    ProductTypeId = 8,
+                    Price = 9.99m,
+                    OriginalPrice = 24.99m,
+                },
+                new ProductVariant
+                {
+                    ProductId = 9,
+                    ProductTypeId = 8,
+                    Price = 14.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 10,
+                    ProductTypeId = 1,
+                    Price = 159.99m,
+                    OriginalPrice = 299m
+                },
+                new ProductVariant
+                {
+                    ProductId = 11,
+                    ProductTypeId = 1,
+                    Price = 79.99m,
+                    OriginalPrice = 399m
                 }
             );
         }
--- Blecommerce/Shared/Product.cs
+++ Blecommerce/Shared/Product.cs
@@ -13,9 +13,8 @@ namespace Blecommerce.Shared
         public string Title { get; set; } = String.Empty;
         public string Description { get; set; } = String.Empty;
         public string ImageUrl { get; set; } = String.Empty;
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal Price { get; set; }
         public Category? Category { get; set; }
         public int CategoryId { get; set; }
+        public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
     }
 }
