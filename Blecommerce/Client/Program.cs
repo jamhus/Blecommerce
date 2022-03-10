3825632371065eb94fdf39c2ec8265e4cc4de572
Jamal Hussein <jamal.hussein@aditro.com>
2022-03-10 14:05:00 +0100

    converted the layout to mudblazor

--- Blecommerce/Client/Program.cs
+++ Blecommerce/Client/Program.cs
@@ -1,6 +1,8 @@
 global using Blecommerce.Shared;
 global using System.Net.Http.Json;
 global using Blecommerce.Client.Services.ProductService;
+global using Blecommerce.Client.Services.CategoryService;
+
 using Blecommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -12,6 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService,ProductService>();
+builder.Services.AddScoped<ICategoryService,CategoryService>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
---
+++ Blecommerce/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,30 @@
+﻿namespace Blecommerce.Client.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly HttpClient _http;
+
+        public CategoryService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Category> categories { get; set; } = new List<Category>();
+
+        public async Task GetCategories()
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/category");
+            if (result != null && result.Data != null)
+                categories = result.Data;
+        }
+
+        public async Task<ServiceResponse<Category>> GetCategory(int id)
+        {
+
+            var result = await _http.GetFromJsonAsync<ServiceResponse<Category>>($"api/product/{id}");
+            if (result != null && result.Data != null)
+                return result;
+            return new ServiceResponse<Category>();
+        }
+    }
+}
