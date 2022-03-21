83426f917d6703b19d99fa62dd46b648e87ff818
Jamal Hussein <jamal.hussein@aditro.com>
2022-03-21 11:35:13 +0100

    Implement Add Order

--- Blecommerce/Client/Services/OrderService/OrderService.cs
+++ Blecommerce/Client/Services/OrderService/OrderService.cs
@@ -1,6 +1,37 @@
-﻿namespace Blecommerce.Client.Services.OrderService
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blecommerce.Client.Services.OrderService
 {
     public class OrderService : IOrderService
     {
+        private readonly HttpClient _http;
+        private readonly AuthenticationStateProvider _auth;
+        private readonly NavigationManager _navigation;
+
+        public OrderService(HttpClient http ,
+            AuthenticationStateProvider auth,
+            NavigationManager navigation
+            )
+        {
+            _http = http;
+            _auth = auth;
+            _navigation = navigation;
+        }
+        public async Task AddOrder()
+        {
+            if (await isAuthenticated ())
+            {
+                await _http.PostAsync("api/order", null);
+
+            } else
+            {
+                _navigation.NavigateTo("login");
+            }
+        }
+
+        private async Task<bool> isAuthenticated()
+        {
+            return (await _auth.GetAuthenticationStateAsync()).User.Identity!.IsAuthenticated;
+        }
     }
 }
