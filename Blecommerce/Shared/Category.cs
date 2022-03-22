f6fbd9b2adbfcfaf0c99a79f656976e73cf7d917
Jamal Hussein <jamal.hussein@aditro.com>
2022-03-22 14:01:22 +0100

    add more props to categories

--- Blecommerce/Shared/Category.cs
+++ Blecommerce/Shared/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +13,11 @@ namespace Blecommerce.Shared
         public string Name { get; set; } = String.Empty;
         public string Url { get; set; } = String.Empty;
         public string Icon { get; set; } = String.Empty;
+        public bool Visible { get; set; } = true;
+        public bool Deleted { get; set; } = false;
+        [NotMapped]
+        public bool Editing { get; set; } = false;
+        [NotMapped]
+        public bool ISNew { get; set; } = false; 
     }
 }
